ec85b45c4f82d243b188d4a42215d81f65fe3df4
weinsh88 <elliottweinshenker@gmail.com>
2025-02-04 15:17:56 -0500

    various tests

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,11 @@
 package codecs
 
 import codecs.Json.JString
+import codecs.Json.JObject
 
 case class Person(name: String, age: Int)
 // case class CarBuyer(name: String, age: Int, car: Option[String])
 
-// Person companion object
 object Person extends PersonCodecs
 
 trait PersonCodecs:
@@ -23,7 +23,16 @@ end PersonCodecs
 
 case class Contacts(people: List[Person])
 object Contacts extends ContactsCodecs
-trait ContactsCodecs
+
+trait ContactsCodecs:
+  given Encoder[Contacts] = ObjectEncoder
+    .field[List[Person]]("people")
+    .encodeAs[Contacts](contacts => contacts.people)
+  given Decoder[Contacts] =
+    Decoder
+      .field[List[Person]]("people")
+      .decodeAs[Contacts](people => Contacts(people))
+end ContactsCodecs
 
 @main def run(): Unit =
   import Json.*
@@ -33,11 +42,12 @@ trait ContactsCodecs
   )
   println(render(jsonObj)) // prints `String` from facade pattern
   val personAby = Person("Aby", 25)
+  val personJohn = Person("John", 30)
   println(render(personAby))
 
-  val personJohn = """{"name":"John", "age":30}"""
+  val person = """{"name":"John", "age":30}"""
   val notPerson = """{"name": "Hello", "age": "2A"}"""
-  val maybeJson = parseJson(personJohn)
+  val maybeJson = parseJson(person)
   println(maybeJson.flatMap(_.decodeAs[String]))
   println(maybeJson.flatMap(_.decodeAs[Person]).get)
 
@@ -46,6 +56,12 @@ trait ContactsCodecs
   // println(jsonObj.en)
 
   val carBuyer = """{"name":"John", "age":30, "car":null}"""
+
+  val contacts = Contacts(List(personAby, personJohn))
+  println(render(contacts))
+  val maybeJsonContact =
+    """{"people": [{"name":"John", "age":30}, {"name":"Aby", "age":25}]}"""
+  println(parseJson(maybeJsonContact).flatMap(_.decodeAs[Contacts]).get)
 
   println(msg)
 
